e0908d06aba68b0597353dc03aaab9733051f6dc
DakshBhamra890 <bhamradaksh@gmail.com>
2025-03-26 20:48:39 -0400

    Commit 2
    made the header and Home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { TransactionComponent } from './components/transaction/transaction.component';
 import { CategoryComponent } from './components/category/category.component';
 
 export const routes: Routes = [
+    {path: "/", component: HomeComponent},
     {path:"transaction", component: TransactionComponent},
     {path:"category", component: CategoryComponent}
 ];
